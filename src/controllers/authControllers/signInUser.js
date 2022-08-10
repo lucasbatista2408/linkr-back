a72e60e9f31e0182052718daa89ad6e554c3ab33
Luis <lf.mc.almeida@poli.ufrj.br>
2022-08-10 04:15:40 -0300

    fix some dependencies issues after change in folde names

--- src/controllers/authControllers/signInUser.js
+++ src/controllers/authControllers/signInUser.js
@@ -1,4 +1,4 @@
-import { userRepo } from '../../repositories/userRepo.js';
+import { userRepo } from '../../repositories/authRepo.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
