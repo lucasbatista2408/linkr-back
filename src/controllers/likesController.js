3b18aae3f568e365ec00b63ab39e7dd65e5692b5
Paloma <casagrande.paloma@gmail.com>
2022-08-10 16:06:36 -0300

    [feat] (get /posts/:id/likes) creates getLikes function that returns the quantity of likes of a post and the people who liked it

--- src/controllers/likesController.js
+++ src/controllers/likesController.js
@@ -3,10 +3,37 @@ import client from "../database/db.js";
 export async function getLikes(req,res){
     const {id} = req.params;
     if(isNaN(parseInt(id))) return res.sendStatus(400);
+
+    const userId =5;
+
     try {
         
-        res.sendStatus(200)
-    } catch (error) {
+        const {rowCount, rows: users} = await client.query(`SELECT u.id, u.username
+        FROM likes l
+        JOIN posts p ON l."postId" = p.id
+        JOIN users u ON l."userId" = u.id
+        WHERE p.id = $1;`, [id]);
+        
+        const hasUserLiked = users.filter(e => e.id === userId);
+        
+        console.log(rowCount, users, hasUserLiked);
+        if(hasUserLiked.length != 0){
+            const usersThatLiked = users.filter(e => e.id != userId);
+            const usernamesThatLiked = usersThatLiked.map(e => e.username);
+            console.log(usersThatLiked, usernamesThatLiked);
+            
+            const likes = {quantity: rowCount, users: usernamesThatLiked };
+            return res.status(200).send(likes);
+        }
+
+       
+        const usernamesThatLiked = users.map(e => e.username);
+        console.log( usernamesThatLiked);
+        
+        const likes = {quantity: rowCount, users: usernamesThatLiked };
+        res.status(200).send(likes);
+
+    } catch (error) {SELECT
         console.log(error);
         res.sendStatus(500);
     }
