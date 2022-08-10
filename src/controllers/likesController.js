f24b5423325aea0ae135b25babdf6fb59d526129
Paloma <casagrande.paloma@gmail.com>
2022-08-09 21:59:19 -0300

    [feat] ( delete  /posts/:id/likes) deletes like from determined user in post id from the likes table on the db

--- src/controllers/likesController.js
+++ src/controllers/likesController.js
@@ -58,7 +58,36 @@ export async function deleteLike(req,res){
     const {id} = req.params;
     if(isNaN(parseInt(id))) return res.sendStatus(400);
 
+    const userId =5;
+    //const userId = req.user??
+
     try {
+
+        const params = [];
+        const conditions = [];
+        let whereClause = '';
+
+        if(id){
+            params.push(parseInt(id));
+            conditions.push(`likes."postId" = $${params.length}`);
+        };
+
+        if(userId){
+            params.push(parseInt(userId));
+            conditions.push(`likes."userId" = $${params.length}`);
+        };
+        
+        if(params.length > 0) {
+            whereClause += `WHERE ${conditions.join(' AND ')}`;
+        };
+
+        const {rowCount} = await client.query(`SELECT * FROM likes ${whereClause};`, params);
+
+        console.log(rowCount, id, userId, whereClause, params);
+
+        if(rowCount == 0 ) return res.sendStatus(404);
+        
+        await client.query(`DELETE FROM likes ${whereClause};`, params);
         
         res.sendStatus(204);
     } catch (error) {
