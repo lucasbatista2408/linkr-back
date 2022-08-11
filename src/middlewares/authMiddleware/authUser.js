2552afcb1dc8ee4f3d0bb492656d4f3c3aba5c14
lucasbatista2408 <lucasbatista2408@gmail.com>
2022-08-11 11:06:31 -0300

    authentication middleware ready and working

--- src/middlewares/authMiddleware/authUser.js
+++ src/middlewares/authMiddleware/authUser.js
@@ -2,3 +2,25 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+export default async function (req,res,next){
+
+  const { authorization } = req.headers;
+  const token = authorization?.replace('Bearer ', '');
+
+  if(!token) return res.sendStatus(401);
+
+  const secret_key = process.env.ACCESS_TOKEN_KEY;
+
+  try {
+    jwt.verify(token, secret_key, (err, userId) => {
+      if(err) return res.sendStatus(403)
+      req.userId = userId;
+      next()
+    })
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(500)
+  }
+
+}
