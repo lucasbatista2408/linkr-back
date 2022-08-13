94d5ca1a12c8dbc24b814f47503e1b7d07b3c930
Luis <lf.mc.almeida@poli.ufrj.br>
2022-08-13 03:21:35 -0300

    fix route user to avoid ambiguity

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,12 @@
 import { pageRepository } from '../repositories/userRepository.js';
 
+
 export async function searchUserControler(req, res){
 	const percent = '%';
-	const {username} = req.params;
+	const {username} = req.query;
 	const id = req.userId;
 	const searchUsername = username?.concat(percent);
+	console.log(username);
 	try{
 		const searchedUser = await pageRepository.searchUserQuerie(searchUsername);
 		if(searchedUser.rowCount === 0){
@@ -13,6 +15,25 @@ export async function searchUserControler(req, res){
 		}
 		const users = searchedUser.rows.filter((item)=> item.id !== id );
 		res.status(200).send(users);
+	}catch(error){
+		res.status(500).send(error);
+	}
+}
+
+export async function searchUserById (req, res){
+	const id = parseInt(req.params.id);
+	if(isNaN(id)){
+		res.status(422).send('Not a number');
+		return;
+	}
+	console.log(id);
+	try{
+		const postByUser = await pageRepository.getPostById(id);
+		if(postByUser.rowCount === 0){
+			res.status(200).send('');
+			return;
+		}
+		res.status(200).send(postByUser.rows);
 	}catch(error){
 		res.status(500).send(error);
 	}
--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -5,8 +5,21 @@ async function searchUserQuerie(username){
 	
 	return client.query(`SELECT id, username, "profileImgUrl" 
                          FROM users 
-                         WHERE username LIKE $1;`, [username]);
+                         WHERE username LIKE $1
+                         ORDER BY username ASC;`, [username]);
+}
+
+async function getPostById(id) {
+	return client.query(
+		`SELECT posts.*, users.username, users."profileImgUrl"
+		 FROM posts
+		 JOIN users 
+		 ON users.id = posts."userId"
+         WHERE users.id = $1
+		 ORDER BY posts.id DESC limit 20`,[id]);
+
 }
 export const pageRepository ={
-	searchUserQuerie
+	searchUserQuerie,
+	getPostById
 };
